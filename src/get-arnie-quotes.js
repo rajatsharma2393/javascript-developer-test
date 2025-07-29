6ba72ef1f330292208c1d965bdd4714790133a48
Rajat Sharma <rajatsharma@Rajats-MacBook-Pro.local>
2025-07-29 19:53:09 +1000

    Implement getArnieQuotes using Promise.allSettled

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,30 @@
 const { httpGet } = require('./mock-http-interface');
 
+const parseResponse = (response) => {
+  const { status, body } = response;
+  const { message } = JSON.parse(body);
+
+  if (status === 200) {
+    return { 'Arnie Quote': message };
+  } else {
+    return { FAILURE: message };
+  }
+};
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const httpPromises = urls.map(url => httpGet(url));
+
+  const settledPromisesResult = await Promise.allSettled(httpPromises);
+  
+  const arnieQuotes = settledPromisesResult.map(result => {
+    if (result.status === 'fulfilled') {
+      return parseResponse(result.value);
+    } else {
+      return { FAILURE: 'Request failed' };
+    }
+  });
+
+  return arnieQuotes;
 };
 
 module.exports = {
